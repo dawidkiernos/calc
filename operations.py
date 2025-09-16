603cbe82c52cce64c35d3e71d9ea895742e03552
KIERNOS Dawid (INTERN) <DKS4@dsone.3ds.com>
2025-09-16 21:32:27 +0200

    Pierwszy commit

---
+++ operations.py
@@ -0,0 +1,8 @@
+def addition(a,b):
+    return a+b
+
+#def substraction(a,b):
+    # todo
+
+#def multiplication(a,b):
+    # todo
---
+++ start.py
@@ -0,0 +1,6 @@
+import operations
+
+a = int(input('pierwsza liczba: '))
+b = int(input('druga liczba: '))
+
+print(operations.addition(a,b))
